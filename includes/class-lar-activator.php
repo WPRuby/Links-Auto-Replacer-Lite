38770ea7d9568b006ed75a81b24eb71865132530
Waseem Senjer <waseem.senjer@gmail.com>
2015-05-17 15:15:03 +0300

    importing the old data

--- includes/class-lar-activator.php
+++ includes/class-lar-activator.php
@@ -30,7 +30,45 @@ class Links_Auto_Replacer_Activator {
 	 * @since    2.0.0
 	 */
 	public static function activate() {
+		self::import_old_data();
+	}
 
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    2.0.0
+	 */
+	private function import_old_data(){
+
+		if(get_option('lar_old_data_imported') === 'yes') return; 
+
+		global $wpdb;
+		$table_name = $wpdb->prefix .'lar_links';
+
+		$old_links = $wpdb->get_results('SELECT * FROM '.$table_name);
+
+		if($old_links === false) return;
+
+		foreach ($old_links as $link){
+			$args = array();
+			$args['post_title'] = $link->keyword;
+			$args['post_status'] = 'publish';
+			$args['post_type'] = 'lar_link';
+			$args['post_date'] = date('Y-m-d H:i:s', $link->created);
+			$link_id = wp_insert_post($args);
+			add_post_meta($link_id, PLUGIN_PREFIX . 'keywords', explode(',', $link->keyword));
+			add_post_meta($link_id, PLUGIN_PREFIX . 'url', $link->keyword_url);
+			add_post_meta($link_id, PLUGIN_PREFIX . 'do_follow', $link->dofollow);
+			add_post_meta($link_id, PLUGIN_PREFIX . 'open_in', $link->open_in);
+			add_post_meta($link_id, PLUGIN_PREFIX . 'shrink', $link->cloack);
+			add_post_meta($link_id, PLUGIN_PREFIX . 'slug', $link->slug);
+			add_post_meta($link_id, PLUGIN_PREFIX . 'link_type', 'external');
+			@add_post_meta($link_id, PLUGIN_PREFIX . 'is_sensitive', $link->is_sensitive);
+
+		}
+		add_option('lar_old_data_imported','yes');
 	}
 
 }
