7dd6d4ee3ddb57e7a2d7f1c37f02687ec1790532
Waseem Senjer <waseem.senjer@gmail.com>
2015-05-19 16:13:05 +0300

    fixing the duplicate in stats

--- admin/class-lar-admin.php
+++ admin/class-lar-admin.php
@@ -385,10 +385,11 @@ class Links_Auto_Replacer_Admin {
 	 * @since    2.0.0
 	 */
 	public function disable_for_single_post(){
+		global $lar_name;
 		$screens = apply_filters('lar_disable_box_screens',array( 'post', 'page' ));
 
 	    foreach ( $screens as $screen ) {
-	        add_meta_box( 'lar_meta', __( 'Disable '.LAR_NAME.' for this post', 'links-auto-replacer' ), array($this,'lar_meta_callback'), $screen );
+	        add_meta_box( 'lar_meta', __( 'Disable '. $lar_name .' for this post', 'links-auto-replacer' ), array($this,'lar_meta_callback'), $screen );
 	    }
 	}
 
