b3e42deed37894a9b58ab488bfd9a6f3a479af05
Waseem Senjer <waseem.senjer@gmail.com>
2015-10-06 19:10:56 +0300

    fix get_final_url

--- admin/classes/class.link.php
+++ admin/classes/class.link.php
@@ -13,16 +13,21 @@ class Lar_Link{
 	**/
 	public static function get_final_url( $link_id ){
 		$link_meta = get_post_meta( $link_id );
-		if($link_meta[PLUGIN_PREFIX.'link_type'][0] == 'external' OR $link_meta[PLUGIN_PREFIX.'link_type'][0] == ''){
+		$link_type = isset($link_meta[PLUGIN_PREFIX.'link_type'][0])?$link_meta[PLUGIN_PREFIX.'link_type'][0]:'';
+		$link_slug = isset($link_meta[PLUGIN_PREFIX.'slug'][0])?$link_meta[PLUGIN_PREFIX.'slug'][0]:'';
+		$link_url = isset($link_meta[PLUGIN_PREFIX.'url'][0])?$link_meta[PLUGIN_PREFIX.'url'][0]:'';
+		$link_internal_url = isset($link_meta[PLUGIN_PREFIX.'internal_url'][0])?$link_meta[PLUGIN_PREFIX.'internal_url'][0]:'';
+		
+		if( $link_type == 'external' OR $link_type == ''){
 				if ( get_option('permalink_structure') != '' ) {
-					$url = ($link_meta[PLUGIN_PREFIX.'slug'][0]!= '')? site_url().'/go/'.$link_meta[PLUGIN_PREFIX.'slug'][0] : $link_meta[PLUGIN_PREFIX.'url'][0];
+					$url = ($link_slug!= '')? site_url().'/go/'.$link_slug : $link_url;
 				
 				}else{
-					$url = ($link_meta[PLUGIN_PREFIX.'slug'][0] != '')? site_url().'/index.php?go='.$link_meta[PLUGIN_PREFIX.'slug'][0] : $link_meta[PLUGIN_PREFIX.'url'][0];
+					$url = ($link_slug != '')? site_url().'/index.php?go='.$link_slug : $link_url;
 				
 				}
-		}elseif($link_meta[PLUGIN_PREFIX.'link_type'][0] == 'internal'){ // if internal link
-					$url = get_permalink($link_meta[PLUGIN_PREFIX.'internal_url'][0]);
+		}elseif($link_type == 'internal'){ // if internal link
+					$url = get_permalink($link_internal_url);
 		}
 		return $url;
 	}
